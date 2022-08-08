350e2166b45eed407b5a743676f62fa6a8abb20e
CJ Lewis <lewis4.christopher@gmail.com>
2022-08-08 06:37:32 -0600

    I'm DONEgit add .git add .

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -164,23 +164,12 @@ router.get('/current', requireAuth, async (req, res) => {
     }
 );
 
-router.get('/:songId', async(req, res, next) => {
+
+
+router.get('/:songId', async(req, res) => {
     const id = req.params.songId
     const song = await Song.findByPk(id)
-
-    return res.json({
-        song
-    })
-})
-
-
-
-
-
-router.get('/:songId', async(req, res, next) => {
-    const id = req.params.songId
-    const song = await Song.findByPk(id)
-
+    console.log("This si the song" + song)
     if (!song){
         res.status(404).json({
             message: "Song couldn't be found",
